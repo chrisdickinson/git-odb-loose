c82637059c9d71ce9165a4ccfe1907712d7d3753
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-03-17 22:46:58 -0700

    parse the buffer into a {type, data} object

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 module.exports = create_loose
 
 var parse_loose = require('./parse')
+  , path = require('path')
+  , TYPE_MAP
+
+TYPE_MAP = { 
+    'commit': 0
+  , 'tree': 1
+  , 'blob': 2
+  , 'tag': 3
+}
 
 create_loose.accept = accept
 
 function create_loose(entry, fs, find_oid, ready) {
   var source = entry.path
-    , prefix_str = /\/([0-9a-fA-F]{2})\//.exec(source)[1]
+    , prefix_str = /\/([0-9a-fA-F]{2})/.exec(source)[1]
     , prefix = parseInt(prefix_str, 16)
 
   ready(null, {
@@ -22,13 +31,15 @@ function create_loose(entry, fs, find_oid, ready) {
     }
 
     var rest = oid.slice(1).toString('hex')
-      , path = path.join('objects', prefix_str, rest)
+      , dir = path.join(source, rest)
 
-    fs.createReadStream(oid.slice(1).toString('hex'))
+    console.log(dir)
+
+    fs.createReadStream(dir)
       .on('error', function(err) { ready(null, undefined) })
       .pipe(parse_loose())
       .on('error', function(err) { ready(err) })
-      .on('data', function(obj) { ready(null, obj) })
+      .on('data', function(obj) { ready(null, parse(obj)) })
 
   }
 
@@ -38,6 +49,24 @@ function create_loose(entry, fs, find_oid, ready) {
 }
 
 function accept(path) {
-  return /objects\/([0-9a-fA-F]{2})\//.test(path)
+  return /objects\/([0-9a-fA-F]{2})$/.test(path)
 }
 
+function parse(buf) {
+  for(var i = 0, len = buf.length; i < len; ++i) {
+    if(buf.readUInt8(i) === 32) {
+      break
+    }
+  } 
+
+  var stridx = i
+    , strtype = buf.slice(0, stridx).toString('utf8')
+
+  for(i += 1; i < len; ++i) {
+    if(buf.readUInt8(i) === 0) {
+      break
+    }
+  }
+
+  return {type: TYPE_MAP[strtype], data: buf.slice(i + 1)}
+}
